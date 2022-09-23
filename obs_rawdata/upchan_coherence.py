7a505f64ccc4b6a4bd51c6d98539699a7e3a7125
savinshynu <savinshynu@gmail.com>
2022-09-23 20:44:07 +0000

    updated version

--- obs_rawdata/upchan_coherence.py
+++ obs_rawdata/upchan_coherence.py
@@ -18,6 +18,7 @@ import matplotlib
 import tqdm as tq
 from matplotlib import pyplot as plt
 import argparse
+from sliding_rfi_flagger import flag_rfi
 
 matplotlib.use('Tkagg')
 
@@ -39,25 +40,32 @@ def main(args):
     nbits = int(header['NBITS'])
     npols = int(header['NPOL'])
     nchan = int(nant_chans/nant)
-    freq_end = header['OBSFREQ'] #stopping frequency 
+    freq_mid = header['OBSFREQ'] #stopping frequency 
     del_t = header['TBIN']
     del_f = header['CHAN_BW']
-    freq_start = freq_end - (nchan*del_f) 
+    freq_start = freq_mid - ((nchan*del_f)/2.0)
+    freq_end = freq_mid + ((nchan*del_f)/2.0)
     freq = np.arange(freq_start, freq_end, del_f) #Coarse channels
     blocksize = header['BLOCSIZE']
     ntsamp_block = int(blocksize/(nant_chans*npols*2*(nbits/8))) # Number of time samples in the block
     ants = header['ANTNMS00']
     ants = ants.split(',')
+    ntsamp_tot = args.tint/del_t #Total number of coarse time samples in the integration time.
+    n_blocks_read = int(ntsamp_tot/ntsamp_block) # Number of blocks to read
     
+    if  n_blocks_read > n_blocks:
+        print(f"Warning: Given intg_time > duration of the file: {round(n_blocks*ntsamp_block*del_t)} s, Using maximum time duration of the file")
+        n_blocks_read = n_blocks
+
     print("The header info for the file: ")
     print(header)
-    print(f"Nblocks: {n_blocks}")
+    print(f"Nblocks: {n_blocks},  Number of blocks to read: {n_blocks_read}")
    
 
     # Collecting data from each block into a big array
-    data = np.zeros((nant, nchan, int(ntsamp_block*n_blocks), npols), dtype = 'complex64')
+    data = np.zeros((nant, nchan, int(ntsamp_block*n_blocks_read), npols), dtype = 'complex64')
     print("Started collecting data")
-    for i in tq.tqdm(range(n_blocks)):
+    for i in tq.tqdm(range(n_blocks_read)):
         head_block, data_block = gob.read_next_data_block()
         data[:,:, i*ntsamp_block:(i+1)*ntsamp_block, :] = data_block.reshape(nant, nchan, ntsamp_block, npols)
 
@@ -124,29 +132,26 @@ def main(args):
     t2 = time.time() 
     print(f"Correlation done in {t2-t1} s")
 
-    if args.tint > ntsamp_new*del_t_new:
-        print("Warning: Given intg_time > duration of the file, Using maximum time duration of the file")
-        tslice = ntsamp_new
 
-    else:
-        tslice = int(args.tint/del_t_new)
-
-    print(f"Averaging data over {tslice*del_t_new} s")    
+    print(f"Averaging data")    
 
     # Average spectra across specified time intervals
-    mean_autocorr_spec1 = np.mean(autocorr1[:tslice,...], axis = 0) #antenna 1 autocorrelation
-    mean_autocorr_spec2 = np.mean(autocorr2[:tslice,...], axis = 0) #antenna 2 autocorrelation
+    mean_autocorr_spec1 = np.mean(autocorr1, axis = 0) #antenna 1 autocorrelation
+    mean_autocorr_spec2 = np.mean(autocorr2, axis = 0) #antenna 2 autocorrelation
 
-    mean_crosscorr_spec = np.mean(cross_corr[:tslice,...], axis = 0) # Cross correlations
+    mean_crosscorr_spec = np.mean(cross_corr, axis = 0) # Cross correlations
 
+    #mid freq range
+    midi = int((nchan*nfine/2) - 16*nfine)
+    mide = int((nchan*nfine/2) + 16*nfine) 
 
     #Plotting the phase and amplitude of the autocorrelation for 2 antennas
 
     fig, axs = plt.subplots(2, 2, constrained_layout=True, figsize = (10,8))
 
-    axs[0,0].plot(freq_new, np.abs(mean_autocorr_spec1[...,0].flatten(order = 'F')), label = 'pol 0')
-    axs[0,0].plot(freq_new, np.abs(mean_autocorr_spec1[...,1].flatten(order = 'F')), label = 'pol 1')
-    axs[0,0].set_ylabel("Amplitude (a.u.)")
+    axs[0,0].plot(freq_new, 10*np.log10(np.abs(mean_autocorr_spec1[...,0].flatten(order = 'F'))), label = 'pol 0')
+    axs[0,0].plot(freq_new, 10*np.log10(np.abs(mean_autocorr_spec1[...,1].flatten(order = 'F'))), label = 'pol 1')
+    axs[0,0].set_ylabel("Amplitude log scale (a.u.)")
     axs[0,0].set_xlabel("Frequency (MHz)")
     axs[0,0].set_title(f"Autocorrelation : {ants[0]}")
     axs[0,0].legend()
@@ -155,12 +160,13 @@ def main(args):
     axs[0,1].plot(freq_new, np.angle(mean_autocorr_spec1[...,1].flatten(order = 'F'), deg = True), '.', label = 'pol 1')
     axs[0,1].set_ylabel("Phase (degrees)")
     axs[0,1].set_xlabel("Frequency (MHz)")
+    axs[0,1].set_xlim(freq_new[midi], freq_new[mide])
     axs[0,1].set_title(f"Autocorrelation : {ants[0]}")
     axs[0,1].legend()
 
-    axs[1,0].plot(freq_new, np.abs(mean_autocorr_spec2[...,0].flatten(order = 'F')), label = 'pol 0')
-    axs[1,0].plot(freq_new, np.abs(mean_autocorr_spec2[...,1].flatten(order = 'F')), label = 'pol 1')
-    axs[1,0].set_ylabel("Amplitude (a.u.)")
+    axs[1,0].plot(freq_new, 10*np.log10(np.abs(mean_autocorr_spec2[...,0].flatten(order = 'F'))), label = 'pol 0')
+    axs[1,0].plot(freq_new, 10*np.log10(np.abs(mean_autocorr_spec2[...,1].flatten(order = 'F'))), label = 'pol 1')
+    axs[1,0].set_ylabel("Amplitude log scale (a.u.)")
     axs[1,0].set_xlabel("Frequency (MHz)")
     axs[1,0].set_title(f"Autocorrelation : {ants[1]}")
     axs[1,0].legend()
@@ -169,6 +175,7 @@ def main(args):
     axs[1,1].plot(freq_new, np.angle(mean_autocorr_spec2[...,1].flatten(order = 'F'), deg = True), '.', label = 'pol 1')
     axs[1,1].set_ylabel("Phase (degrees)")
     axs[1,1].set_xlabel("Frequency (MHz)")
+    axs[1,1].set_xlim(freq_new[midi], freq_new[mide])
     axs[1,1].set_title(f"Autocorrelation : {ants[1]}")
     axs[1,1].legend()
 
@@ -185,25 +192,27 @@ def main(args):
 
     fig, axs = plt.subplots(2, 2, constrained_layout=True, figsize = (10,8))
 
-    axs[0,0].plot(np.angle(mean_crosscorr_spec[...,0].flatten(order = 'F'), deg = True),  '.', label = 'pol 0')
+    axs[0,0].plot(freq_new, np.angle(mean_crosscorr_spec[...,0].flatten(order = 'F'), deg = True),  '.', label = 'pol 0')
     axs[0,0].set_ylabel("Phase (degrees)")
-    axs[0,0].set_xlabel("Frequency channels")
-    axs[0,0].set_title(f"Crosscorrelation : {ants[0]}- {ants[1]}")
+    axs[0,0].set_xlabel("Frequency (MHz)")
+    axs[0,0].set_xlim(freq_new[midi], freq_new[mide])
+    axs[0,0].set_title(f"Crosscorrelation : {ants[0]}-{ants[1]}")
     axs[0,0].legend()
 
-    axs[0,1].plot(np.angle(mean_crosscorr_spec[...,1].flatten(order = 'F'), deg = True), '.', label = 'pol 1')
+    axs[0,1].plot(freq_new, np.angle(mean_crosscorr_spec[...,1].flatten(order = 'F'), deg = True), '.', label = 'pol 1')
     axs[0,1].set_ylabel("Phase (degrees)")
-    axs[0,1].set_xlabel("Frequency channels")
-    axs[0,1].set_title(f"Crosscorrelation : {ants[0]}- {ants[1]}")
+    axs[0,1].set_xlabel("Frequency (MHz)")
+    axs[0,1].set_xlim(freq_new[midi], freq_new[mide])
+    axs[0,1].set_title(f"Crosscorrelation : {ants[0]}-{ants[1]}")
     axs[0,1].legend()
 
-    axs[1,0].plot(freq_new, np.abs(mean_crosscorr_spec[...,0].flatten(order = 'F')), label = 'pol 0')
+    axs[1,0].plot(freq_new, 10*np.log10(np.abs(mean_crosscorr_spec[...,0].flatten(order = 'F'))), label = 'pol 0')
     axs[1,0].set_ylabel("Amplitude (a.u.)")
     axs[1,0].set_xlabel("Frequency (MHz)")
     axs[1,0].set_title(f"Crosscorrelation : {ants[0]}-{ants[1]}")
     axs[1,0].legend()
 
-    axs[1,1].plot(freq_new, np.abs(mean_crosscorr_spec[...,1].flatten(order = 'F')), label = 'pol 1')
+    axs[1,1].plot(freq_new, 10*np.log10(np.abs(mean_crosscorr_spec[...,1].flatten(order = 'F'))), label = 'pol 1')
     axs[1,1].set_ylabel("Amplitude (a.u.)")
     axs[1,1].set_xlabel("Frequency (MHz)")
     axs[1,1].set_title(f"Crosscorrelation : {ants[0]}-{ants[1]}")
@@ -211,11 +220,77 @@ def main(args):
     
     fig.suptitle(f"File: {args.dat_file}")
 
-    if args.plot:
+    if  args.plot:
         plt.show()
     else:
         plt.savefig(f"cross_corr_{os.path.basename(args.dat_file)}.png", dpi = 150)
         plt.close()
+
+    
+
+    #Conduct an ifft of the crosscorrelated spectra to get the time delay plots
+    if  args.time_delay:
+        
+        
+        mean_crosscorr_pol0 = mean_crosscorr_spec[...,0].flatten(order = 'F')
+        mean_crosscorr_pol1 = mean_crosscorr_spec[...,1].flatten(order = 'F')
+
+        spec_mean_crosscorr_pol0 = np.abs(mean_crosscorr_pol0)
+        spec_mean_crosscorr_pol1 = np.abs(mean_crosscorr_pol1)
+        
+        #Conducting a step of RFI removal using sliding median window before doing ifft
+        # Threshold for RFI removal
+        threshold = 3
+        
+        #Getting bad channels
+        bad_chan0 = flag_rfi(spec_mean_crosscorr_pol0, int(nfine/6), threshold)
+        bad_chan1 = flag_rfi(spec_mean_crosscorr_pol1, int(nfine/6), threshold)
+        
+        print(bad_chan0.shape[0], bad_chan1.shape[0])
+        
+        ##Zeroing bad channels
+        mean_crosscorr_pol0[bad_chan0[:,0]] = 0
+        mean_crosscorr_pol1[bad_chan1[:,0]] = 0
+
+        #FFT of the spectra
+        mean_crosscorr_pol0_ifft = np.fft.ifft(mean_crosscorr_pol0)
+        mean_crosscorr_pol1_ifft = np.fft.ifft(mean_crosscorr_pol1) 
+        
+        #FFT shift of the data
+        mean_crosscorr_pol0_ifft = np.fft.ifftshift(mean_crosscorr_pol0_ifft)
+        mean_crosscorr_pol1_ifft = np.fft.ifftshift(mean_crosscorr_pol1_ifft)
+        
+
+
+        #Defining  total frequency channels and fine channel bandwidths in Hz to get the time lags
+        tlags = np.fft.fftfreq(nchan*nfine,(del_f/nfine)*1e+6)
+        tlags = np.fft.fftshift(tlags)*1e+9 #Converting the time lag into us
+        tmax_pol0 = np.argmax(10*np.log(np.abs(mean_crosscorr_pol0_ifft)))
+        tmax_pol1 = np.argmax(10*np.log(np.abs(mean_crosscorr_pol1_ifft)))
+
+        fig, (ax0, ax1) = plt.subplots(1, 2, constrained_layout=True, figsize = (10,8))
+
+        ax0.plot(tlags, 10*np.log(np.abs(mean_crosscorr_pol0_ifft)), label = 'pol 0')
+        ax0.set_ylabel("Power (a.u) log scale")
+        ax0.set_xlabel(f"Time lags (delta t = {tlags[1] -tlags[0]}) ns)")
+        ax0.set_title(f"{ants[0]}-{ants[1]}: time delay = {tlags[tmax_pol0]} ns")
+        ax0.legend()
+
+        ax1.plot(tlags, 10*np.log(np.abs(mean_crosscorr_pol1_ifft)), label = 'pol 1')
+        ax1.set_ylabel("Power (a.u) log scale")
+        ax1.set_xlabel(f"Time lags (delta t = {tlags[1] -tlags[0]} ns)")
+        ax1.set_title(f"{ants[0]}-{ants[1]}: time delay = {tlags[tmax_pol1]} ns")
+        ax1.legend()
+
+        fig.suptitle(f"File: {args.dat_file}")
+
+        if args.plot:
+            plt.show()
+        else:
+            plt.savefig(f"time_delay_{os.path.basename(args.dat_file)}.png", dpi = 150)
+            plt.close()
+
+
 
 
     #Proceed if needed to track a channel as a function of time
@@ -263,9 +338,10 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-d','--dat_file', type = str, required = True, help = 'GUPPI raw file to read in')
     parser.add_argument('-f','--lfft', type = int, required = True, default = 120,  help = 'Length of FFT, default:120')
-    parser.add_argument('-i', '--tint', type = int, required = True, help = 'Time to integrate in (s)')
+    parser.add_argument('-i', '--tint', type = float, required = True, help = 'Time to integrate in (s), default: whole file duration')
+    parser.add_argument('-td', '--time_delay', action = 'store_true', help = 'If there are fringes, plot/save the time delay plot. An RFI filtering is conductted before the IFFT')
     parser.add_argument('-p', '--plot', action = 'store_true', help = 'plot the figures, otherwise save figures to working directory')
-    parser.add_argument('-t', '--track', action = 'store_true', help = 'Track a channel as a function of time, need to enter a RFI free channel after inspection. Use this only if plotting is enabled.')
+    parser.add_argument('-t', '--track', action = 'store_true', help = 'Track a channel as a function of time, need to enter a RFI free channel after inspection')
 
     args = parser.parse_args()
 
